cad641f18b3597f09dd796eba895a70465a81d2f
Sano Yusuke <sano.yusuke@tecotec.co.jp>
2017-09-20 14:47:25 +0900

    [Add] 指紋認証機能のテスト実装

--- app/src/main/java/jp/co/hokuto/weatherreport/activity/MainActivity.kt
+++ app/src/main/java/jp/co/hokuto/weatherreport/activity/MainActivity.kt
@@ -1,10 +1,16 @@
 package jp.co.hokuto.weatherreport.activity
 
+import android.content.Context
+import android.hardware.fingerprint.FingerprintManager
+import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
 import jp.co.hokuto.weatherreport.R
 import jp.co.hokuto.weatherreport.activity.fragment.MainFragment
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,11 +19,32 @@ class MainActivity : AppCompatActivity() {
 
 		setContentView(R.layout.activity_main)
 
-		val fragment : Fragment = MainFragment()
-		val transaction = supportFragmentManager.beginTransaction()
-		transaction.add(R.id.fragment_container, fragment)
-		transaction.addToBackStack(null)
-		transaction.commit()
+		// TODO:指紋認証テスト
+		if (Build.VERSION.SDK_INT >= 23) {
+			val fingerprintManager = getSystemService(Context.FINGERPRINT_SERVICE) as FingerprintManager
+
+			try {
+				if (fingerprintManager.isHardwareDetected) {
+					if (fingerprintManager.hasEnrolledFingerprints()) {
+						Toast.makeText(this.applicationContext, "指紋登録されています。", Toast.LENGTH_SHORT).show()
+
+						fingerprintManager.authenticate(null, null, 0, fingerManagerCallback(), null)
+
+					} else {
+						Toast.makeText(this.applicationContext, "指紋登録されていません。", Toast.LENGTH_SHORT).show()
+					}
+				} else {
+					Toast.makeText(this.applicationContext, "指紋認証が搭載されていない端末です。", Toast.LENGTH_SHORT).show()
+				}
+
+			} catch (e : SecurityException) {
+				Log.e("test", "エラー：" + e)
+			}
+
+		} else {
+			goToMainFragment()
+		}
+
 	}
 
 
@@ -37,4 +64,57 @@ class MainActivity : AppCompatActivity() {
 	override fun onDestroy() {
 		super.onDestroy()
 	}
+
+
+	/**
+	 * メインFragment画面への遷移
+	 */
+	private fun goToMainFragment() {
+		val fragment : Fragment = MainFragment()
+		val transaction = supportFragmentManager.beginTransaction()
+		transaction.add(R.id.fragment_container, fragment)
+		transaction.addToBackStack(null)
+		transaction.commit()
+	}
+
+
+	private fun fingerManagerCallback() : FingerprintManager.AuthenticationCallback {
+		val listener : FingerprintManager.AuthenticationCallback = object : FingerprintManager.AuthenticationCallback() {
+			/**
+			 * エラー
+			 */
+			override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+				super.onAuthenticationError(errorCode, errString)
+
+				Log.d("test", "(DEBUG)エラー")
+			}
+
+			/**
+			 * 指紋認証 失敗
+			 */
+			override fun onAuthenticationFailed() {
+				super.onAuthenticationFailed()
+
+				Log.d("test", "(DEBUG)指紋認証 失敗")
+			}
+
+			/**
+			 * 指紋認証 成功
+			 */
+			override fun onAuthenticationSucceeded(result: FingerprintManager.AuthenticationResult?) {
+				super.onAuthenticationSucceeded(result)
+
+				Log.d("test", "(DEBUG)指紋認証 成功")
+
+				goToMainFragment()
+			}
+
+			override fun onAuthenticationHelp(helpCode: Int, helpString: CharSequence?) {
+				super.onAuthenticationHelp(helpCode, helpString)
+			}
+		}
+
+		return listener
+	}
+
 }
